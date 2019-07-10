a9a68c8b549436854c0c5bb0278275e02f30c093
Jack B <jbolous@gmail.com>
2019-07-09 19:36:25 -0500

    Added JUnit tests with Mockito

---
+++ src/test/java/com/bolous/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.bolous.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setup(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+
+        Long id = 4L;
+        category.setId(id);
+
+        assertEquals(id, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
