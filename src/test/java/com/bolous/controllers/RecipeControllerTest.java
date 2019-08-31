71a309313f676c26e4fca6dc5949311b16615ded
Jack B <jbolous@gmail.com>
2019-08-31 14:37:39 -0500

    Added validation on recipe

--- src/test/java/com/bolous/controllers/RecipeControllerTest.java
+++ src/test/java/com/bolous/controllers/RecipeControllerTest.java
@@ -65,7 +65,23 @@ public class RecipeControllerTest {
     }
 
     @Test
-    public void TestPostNewRecipeForm() throws Exception {
+    public void testPostNewRecipeForm() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions", "some directions"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception{
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
 
@@ -75,8 +91,7 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/recipe/2/show"));
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
